e8fc0537bddca29674876fdf8ca41222bc6ad0af
JSpiner <jspiner@naver.com>
2017-01-18 13:31:15 +0900

    add socket

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from slackclient import SlackClient
 import json
 import requests
-
+import time
 
 with open('key.json') as json_data:
     d = json.load(json_data)
@@ -10,18 +10,59 @@ with open('key.json') as json_data:
 
 print("start")
 
+def requestNlp(query):
+    result = requests.get(
+        "https://api.api.ai/v1/query",
+        params = {
+            'v' : '20150910',
+            'query' : query,
+            'lang' : 'kr',
+            'sessionId' : '1'
+        },
+        headers = {
+            'Authorization' : ai_token,
+            'COntent-Type' : 'application/json; charset=utf-8'
+        }
+    )
+    print(result.text)
+    return result.json()
 
-result = requests.get(
-    "https://api.api.ai/v1/query",
-    params = {
-        'v' : '20150910',
-        'query' : '오늘 3회의실 3시부터 4시까지 예약해줘',
-        'lang' : 'kr',
-        'sessionId' : '1'
-    },
-    headers = {
-        'Authorization' : ai_token,
-        'COntent-Type' : 'application/json; charset=utf-8'
-    }
+slackClient = SlackClient(bot_token)
+
+
+data = slackClient.api_call(
+    "users.info",
+    bot="hotsan"
 )
-print(result.text)
+
+print(data)
+print("try connect")
+if slackClient.rtm_connect():
+    print("connected")
+
+    slackClient.api_call(
+        "chat.postMessage",
+        channel="#testbed",
+        text="Hello from Python! :tada:"
+    )
+    while True:
+        response = slackClient.rtm_read()
+        if len(response) == 0:  
+            continue
+
+        for data in response:
+            if data['type'] == "message" and 'subtype' not in data:
+                if data['text'][0] == '/':
+                    continue
+                if "<@U3GUQSAR3>" in data['text']:
+                    text = data['text']
+                    text = text.replace("<@U3GUQSAR3>","").strip()
+
+                    responseJson = requestNlp(text)
+                    slackClient.api_call(
+                        "chat.postMessage",
+                        channel=data['channel'],
+                        text=responseJson['result']['fulfillment']['messages'][0]['speech']
+                    )
+                    print(text)
+
